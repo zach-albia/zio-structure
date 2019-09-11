dafafd4fa12181940d6f6a0071468561139bf8cf
Zach Albia <zach.albia@gmail.com>
2019-09-11 15:40:40 +0800

    feat: add Foo repo

---
+++ src/main/scala/domain/FooRepository.scala
@@ -0,0 +1,39 @@
+package domain
+
+import zio.{Ref, UIO}
+
+import scala.language.higherKinds
+
+trait FooRepository[F[_]] {
+
+  val fooRepository: FooRepository.Service[F]
+}
+
+object FooRepository {
+
+  trait Service[F[_]] {
+
+    def save(name: String): F[Foo]
+
+    def fetch(id: String): F[Option[Foo]]
+
+    def delete(id: String): F[Unit]
+  }
+
+  final case class InMemoryFooRepository(ref: Ref[Map[String, Foo]],
+                                         counter: Ref[Long])
+      extends Service[UIO] {
+    override def save(name: String): UIO[Foo] =
+      for {
+        newId <- counter.update(_ + 1) map { _.toString }
+        foo = Foo(newId, name)
+        _ <- ref.update(store => store + (newId -> foo))
+      } yield foo
+
+    override def fetch(id: String): UIO[Option[Foo]] =
+      ref.get.map(_.get(id))
+
+    override def delete(id: String): UIO[Unit] =
+      ref.update(_ - id).unit
+  }
+}
