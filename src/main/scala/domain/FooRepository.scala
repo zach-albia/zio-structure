272c09cef10a1da7c0dc326e32063d0a29cfd348
Zach Albia <zach.albia@gmail.com>
2019-09-12 11:29:34 +0800

    feat: split FooRepository#save into create and update

--- src/main/scala/domain/FooRepository.scala
+++ src/main/scala/domain/FooRepository.scala
@@ -13,19 +13,23 @@ object FooRepository {
 
   trait Service[F[_]] {
 
-    def save(name: String): F[Foo]
+    def create(name: String): F[Foo]
 
     def fetch(id: String): F[Option[Foo]]
+
+    def update(id: String, name: String): F[Option[Foo]]
 
     def delete(id: String): F[Unit]
   }
 
-  final case class InMemoryFooRepository(ref: Ref[Map[String, Foo]],
-                                         counter: Ref[Long])
-      extends Service[UIO] {
-    override def save(name: String): UIO[Foo] =
+  final case class InMemoryFooRepository(
+      ref: Ref[Map[String, Foo]],
+      counter: Ref[Long]
+  ) extends Service[UIO] {
+
+    override def create(name: String): UIO[Foo] =
       for {
-        newId <- counter.update(_ + 1) map { _.toString }
+        newId <- counter.update(_ + 1).map(_.toString)
         foo = Foo(newId, name)
         _ <- ref.update(store => store + (newId -> foo))
       } yield foo
@@ -33,7 +37,16 @@ object FooRepository {
     override def fetch(id: String): UIO[Option[Foo]] =
       ref.get.map(_.get(id))
 
+    override def update(id: String, name: String): UIO[Option[Foo]] =
+      for {
+        updatedFooOpt <- ref.get.map(_.get(id).map(_ => Foo(id, name)))
+        _ <- updatedFooOpt
+          .map(foo => ref.update(store => store + (id -> foo)))
+          .getOrElse(UIO.unit)
+      } yield updatedFooOpt
+
     override def delete(id: String): UIO[Unit] =
       ref.update(_ - id).unit
   }
+
 }
