358cb728c120ca273f58222edb8e6b967ac1bb8d
Zach Albia <zach.albia@gmail.com>
2019-09-16 08:34:48 +0800

    refactor: extract Environment type bound into FooService.Service type

--- src/main/scala/domain/FooService.scala
+++ src/main/scala/domain/FooService.scala
@@ -9,9 +9,10 @@ object FooService {
   type Environment[F[_], S, E] = FooRepository[F] with HasFunctionK[F, ZIO[S, E, *]]
 
   trait Service [R <: Environment[F, S, E], F[_], S, E] {
+    type EnvBound = Environment[F, S, E]
 
-    def createFoo(name: String): ZIO[S with Environment[F, S, E], E, Foo] = for {
-      env <- ZIO.environment[Environment[F, S, E]]
+    def createFoo(name: String): ZIO[S with EnvBound, E, Foo] = for {
+      env <- ZIO.environment[EnvBound]
       foo <- env.functionK(env.fooRepository.create(name))
     } yield foo
   }
